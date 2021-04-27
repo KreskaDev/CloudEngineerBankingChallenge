2ce55da09c2368b60deaebecdcac3cddb2d8230a
Rafal <kreskadev@gmail.com>
2021-04-27 12:55:15 +0200

    Add error handling

---
+++ src/Shared/Cebc.Shared.Infrastructure/Modules/Extensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Cebc.Shared.Abstractions.Modules;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Cebc.Shared.Infrastructure.Modules
+{
+    public static class Extensions
+    {
+        public static IServiceCollection AddModuleInfo(this IServiceCollection services, IList<IModule> modules)
+        {
+            var moduleInfoProvider = new ModuleInfoProvider();
+            var moduleInfo =
+                modules?.Select(x => new ModuleInfo(x.Name, x.Path, x.Policies ?? Enumerable.Empty<string>())) ??
+                Enumerable.Empty<ModuleInfo>();
+            moduleInfoProvider.Modules.AddRange(moduleInfo);
+            services.AddSingleton(moduleInfoProvider);
+
+            return services;
+        }
+
+        public static void MapModuleInfo(this IEndpointRouteBuilder endpoint)
+        {
+            endpoint.MapGet("modules", context =>
+            {
+                var moduleInfoProvider = context.RequestServices.GetRequiredService<ModuleInfoProvider>();
+                return context.Response.WriteAsJsonAsync(moduleInfoProvider.Modules);
+            });
+        }
+
+        public static IHostBuilder ConfigureModules(this IHostBuilder builder)
+            => builder.ConfigureAppConfiguration((ctx, cfg) =>
+            {
+                foreach (var settings in GetSettings("*"))
+                {
+                    cfg.AddJsonFile(settings);
+                }
+
+                foreach (var settings in GetSettings($"*.{ctx.HostingEnvironment.EnvironmentName}"))
+                {
+                    cfg.AddJsonFile(settings);
+                }
+
+                IEnumerable<string> GetSettings(string pattern)
+                    => Directory.EnumerateFiles(ctx.HostingEnvironment.ContentRootPath,
+                        $"module.{pattern}.json", SearchOption.AllDirectories);
+            });
+    }
+}
---
+++ src/Shared/Cebc.Shared.Infrastructure/Modules/ModuleInfoProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Cebc.Shared.Infrastructure.Modules
+{
+    public class ModuleInfoProvider
+    {
+        public List<ModuleInfo> Modules { get; } = new();
+    }
+    public record ModuleInfo(string Name, string Path, IEnumerable<string> Policies);
+}
